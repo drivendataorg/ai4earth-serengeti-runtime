c9d84108a984a61a546cfb331e273e7d98bd1f19
Jay Qi <jayqi@users.noreply.github.com>
2019-10-08 11:18:11 -0500

    Test build failure

--- runtime/tests/test-installs.py
+++ runtime/tests/test-installs.py
@@ -6,23 +6,23 @@ logging.getLogger('').setLevel(logging.INFO)
 logging.info("Testing if Python packages can be loaded correctly.")
 
 packages = [
-    "fastai", 
-    "lightgbm", 
-    "mahotas", 
+    "fastai",
+    "lightgbm",
+    "mahotas",
     "cv2", # opencv
-    "pandas", 
-    "PIL", # pillow 
-    "dotenv", 
+    "pandas",
+    "PIL", # pillow
+    "dotenv",
     "numpy",
     "torch", # pytorch
     "skimage", # scikit-image
-    "sklearn", # scikit-learn 
-    "scipy", 
-    "tensorflow", 
-    "torchvision", 
-    "xgboost", 
+    "sklearn", # scikit-learn
+    "scipy",
+    "tensorflow",
+    "torchvision",
+    "xgboost",
     ## ADD ADDITIONAL REQUIREMENTS BELOW HERE ##
-
+    "foobarnotarealpackage"
     ############################################
 ]
 
