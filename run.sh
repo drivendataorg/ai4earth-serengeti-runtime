3808ea7aeb8278c95dfdc1a22e1aa5f841691d2b
Jay Qi <jayqi@users.noreply.github.com>
2019-10-10 16:32:18 -0500

    Repo Reorganization and Testing in CI (#1)
    * Move runtime files into runtime directory
    
    * File structure for benchmark
    
    * Update r requirements to include futile.logger and pandas
    
    * Removed old test submission files
    
    * New submission prep script
    
    * Update run.sh to remove submission prep
    
    * Add image-files directory as placeholder
    
    * Update README
    
    * Renamed package-install-*.R scripts to match convention
    
    * Update commands to account for runtime directory
    
    * Add testing to CI
    
    * Reconcile R install scripts
    
    * Fix filenames/paths
    
    * Fix paths again. Avoid duplicate builds
    
    * Pipe specifies which test
    
    * Mount tests
    
    * Fixed paths again.
    
    * README polish
    
    * PR requested revisions

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 set -e
 
 # load .env vars
-cat .env | while read a; do export $a; done
+if [ -f .env ]; then
+    cat .env | while read a; do export $a; done
+fi
 
-# prepare example submission
-mkdir -p submission
-cd inference; zip -r ../submission/submission.zip ./*; cd ..
-
+cd runtime
 
 # test configuration
 if [ $(which nvidia-smi) ]
@@ -16,14 +15,14 @@ then
     docker run --env-file .env \
            --gpus all \
            --network none \
-           --mount type=bind,source=$(pwd)/image-files,target=/inference/data,readonly \
+           --mount type=bind,source=$(pwd)/inference-data,target=/inference/data,readonly \
            --mount type=bind,source=$(pwd)/submission,target=/inference/submission \
            ai-for-earth-serengeti/inference
 else
     docker build --build-arg CPU_GPU=cpu -t ai-for-earth-serengeti/inference .
     docker run --env-file .env \
             --network none \
-            --mount type=bind,source=$(pwd)/image-files,target=/inference/data,readonly \
+            --mount type=bind,source=$(pwd)/inference-data,target=/inference/data,readonly \
             --mount type=bind,source=$(pwd)/submission,target=/inference/submission \
             ai-for-earth-serengeti/inference
 fi
